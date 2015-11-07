c48d0543942529493de0ec07f175eeb555d5d410
Jon Bloomer <jon@freaksauce.com>
2015-11-08 09:53:19 +1100

    added an addItem form which passes values to container component, todo: save to collection, integrate file uploads

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -22,9 +22,17 @@ Dashboard = React.createClass({
 			if (this.data.user.profile.role !== 'admin') {
 				{this.redirect()}				
 			}else{
-				return <DashboardContent />
+				return <DashboardContent ref="inner" addItem={this.addItem} />
 			}
 		}
+	},
+
+	addItem(e) {
+		let item_id = this.refs.inner.refs.item_id.getDOMNode().value;
+		let item_name = this.refs.inner.refs.item_name.getDOMNode().value;
+		let item_image = this.refs.inner.refs.item_image.getDOMNode().value;
+		console.log(item_id+', '+item_name+', '+item_image);
+		return false;
 	},
 
 	render() {		
--- client/components/DashboardContent.jsx
+++ client/components/DashboardContent.jsx
@@ -10,24 +10,48 @@ DashboardContent = React.createClass({
 
 	showInventory() {
 		return this.data.inventory.map((item) => {
-	      console.log(item);
 	      return <div className="ui list">
 					<div className="item">
 						<img className="ui image" data-lrg={item.image} src={item.thumb}/>
 						<div className="content">
 							<a href={"/inventory-item/"+item._id} className="header">{item.name}</a>
-							<div class="description">In Stock: {item.inStock? <a className="ui basic label green">Yes</a> : <a className="ui basic label red">No</a> }</div>
+							<div className="description">In Stock: {item.inStock? <a className="ui basic label green">Yes</a> : <a className="ui basic label red">No</a> }</div>
 						</div>
 					</div>
 				</div>
 	    });
 	},
 
+	showAddItemForm() {
+		return  <form className="ui form" onSubmit={this.props.addItem}>
+					<h3>Add new item</h3>
+					<div className="field">
+						<label>Item ID</label>
+						<input ref="item_id" type="text" name="item-id" placeholder="Item ID"/>
+					</div>
+					<div className="field">
+						<label>Item Name</label>
+						<input ref="item_name" type="text" name="item-name" placeholder="Item Name"/>
+					</div>
+					<div className="field">
+						<label>Image upload</label>
+						<input ref="item_image" type="file" name="item-image" placeholder="Item image"/>
+					</div>
+					
+					<button className="ui button" type="submit">Add</button>
+				</form>
+	},
+
 	render() {
 		return (
-			<div>
-				<h3>Inventory list</h3>
-				{this.showInventory()}
+			<div className="ui grid">
+				<div className="eight wide column">
+					<h3>Inventory list</h3>
+					{this.showInventory()}
+				</div>
+				<div className="eight wide column">
+					{this.showAddItemForm()}
+				</div>
 			</div>
 		);
 	}
