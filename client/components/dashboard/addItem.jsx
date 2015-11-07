22038b47b89c3c5589531b46a9fb8311f46b9126
Jon Bloomer <jon@freaksauce.com>
2015-11-08 10:15:14 +1100

    added error message item and set up vars ready to validate form

--- client/components/dashboard/addItem.jsx
+++ client/components/dashboard/addItem.jsx
@@ -1,15 +1,29 @@
 AddItem = React.createClass({
 	
+	getInitialState() {
+		return {
+			showErrorMessage: false,
+			errorObj: {}
+		}
+	},
+
 	addItem(e) {
 		let item_id = this.refs.item_id.getDOMNode().value;
 		let item_name = this.refs.item_name.getDOMNode().value;
 		let item_image = this.refs.item_image.getDOMNode().value;
+		this.validate();
 		console.log(item_id+', '+item_name+', '+item_image);
 		return false;
 	},
 
+	validate(item) {
+		this.setState({errorObj: {heading: 'Error', message: 'Invalid email'}});
+		this.setState({showErrorMessage: true});
+	},
+
 	render() {
 		return  <form className="ui form" onSubmit={this.addItem}>
+					{this.state.showErrorMessage ? <Message messageType="negative" messageContent={this.state.errorObj} hideErrorMessage={this.hideErrorMessage} /> : <p>no error</p>}
 					<h3>Add new item form</h3>
 					<div className="field">
 						<label>Item ID</label>
