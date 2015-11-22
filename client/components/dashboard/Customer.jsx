9b80768747e0a640347f2d5eb052c8b3fa51e694
Jon Bloomer <jon@freaksauce.com>
2015-11-23 08:30:13 +1100

    get customer id from flowrouter in component

--- client/components/dashboard/Customer.jsx
+++ client/components/dashboard/Customer.jsx
@@ -1,5 +1,11 @@
 Customer = React.createClass({
 
+	componentWillMount() {
+		let customerId = FlowRouter.current().params;
+		let customer = CustomersCollection.find({_id: customerId}).fetch();
+		console.log(customer);
+	},
+
 	render() {
 		return (
 			<h3>Customer</h3>
