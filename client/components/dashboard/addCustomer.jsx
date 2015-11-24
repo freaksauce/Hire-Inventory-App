0ed78a9889be42b31fda048f89622c6b8755b732
Jon Bloomer <jon@freaksauce.com>
2015-11-25 08:16:06 +1100

    moved validate customer functionality from AddCustomer into mixin to be shared with edit customer function

--- client/components/dashboard/addCustomer.jsx
+++ client/components/dashboard/addCustomer.jsx
@@ -13,36 +13,19 @@ AddCustomer = React.createClass({
 
 	addCustomer(e) {
 		e.preventDefault();
-		console.log('add customer')
-		let fields = [];
-		for (el in this.refs) {
-			fields.push(el);
-		}
-		let customersArr = [];
-		let errors = {};
-	    fields.forEach(function(field, index) {
-	      let el = this.refs[field];
-	      let value = el.value;
-	      let required = el.required;
-	      if (required && !value) {
-	        errors[field] = "The "+field+" field is required";
-	      }else{
-	      	customersArr.push({field: fields[index], value: value});
-	      }
-	    }.bind(this));
-
-	    if (Object.keys(errors).length !== 0) {
-	    	this.setState({errorObj: {heading: 'Add form errors', message: errors}});
+		console.log('add customer');
+		const returnCustomerObj = this.validateCustomer(this.refs);
+		
+		if (returnCustomerObj.errors) {
+			this.setState({errorObj: {heading: 'Add form errors', message: this.errors}});
 		    this.setState({showErrorMessage: true});
-	    }else{
-	    	// call method to insert into mongo	    	
-	    	console.log('customersArr',customersArr);
-	    	
-	    	Meteor.call('addCustomer', customersArr, (err) => {
+		}else{
+			console.log('customersArr',returnCustomerObj);
+			Meteor.call('addCustomer', returnCustomerObj, (err) => {
 	    		if (err) {
 	    			// console.log(err);
 	    			this.setState({errorObj: {heading: 'Add form errors', message: err.message}});
-		    this.setState({showErrorMessage: true});
+		    		this.setState({showErrorMessage: true});
 	    		}else{
 	    			this.refs.ref_customer_name.value = '';
 	    			this.refs.ref_customer_email.value = '';
@@ -51,7 +34,7 @@ AddCustomer = React.createClass({
 	    			this.setState({showInsertSuccess: true});
 	    		}
 	    	});
-	    }
+		}
 	    
 	},	
 
